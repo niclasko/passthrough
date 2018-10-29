999b684431dc46354eb6cd378c908c8f23bae5d0
niclasko <niclasko@gmail.com>
2018-10-29 11:00:38 +0100

    Change

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
+	header("Access-Control-Allow-Origin: *");
 	try {
 		$content = file_get_contents($_GET["u"]);
 		if ($content === false) {
 			header("HTTP/1.0 404 Not Found");
 			die();
 		} else {
-			header("Access-Control-Allow-Origin: *");
 			echo $content;
 		}
 	} catch (Exception $e) {
