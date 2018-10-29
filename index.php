a53b774e25dd634359262c89fd5d05b67e8ed533
niclasko <niclasko@gmail.com>
2018-10-29 10:59:07 +0100

    404 Not found

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	echo file_get_contents($_GET["u"]);
+	try {
+		$content = file_get_contents($_GET["u"]);
+		if ($content === false) {
+			header("HTTP/1.0 404 Not Found");
+			die();
+		} else {
+			header("Access-Control-Allow-Origin: *");
+			echo $content;
+		}
+	} catch (Exception $e) {
+		header("HTTP/1.0 404 Not Found");
+		die();
+	}
 ?>
